53b34951c3757a5351c591dd5325651173b0da44
HUUQUY1004 <99793142+HUUQUY1004@users.noreply.github.com>
2024-06-02 23:55:00 +0700

    Loadmore cho phone

--- Android/app/src/main/java/com/example/appbanhang/adapter/PhoneAdapter.java
+++ Android/app/src/main/java/com/example/appbanhang/adapter/PhoneAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,10 +18,11 @@ import com.example.appbanhang.model.Product;
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class PhoneAdapter  extends RecyclerView.Adapter<PhoneAdapter.MyViewHolder>{
+public class PhoneAdapter  extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     Context context;
     List<Product> listProduct;
-
+    private static  final int VIEW_TYPE_DATA = 0 ;
+    private static  final  int VIEW_TYPE_lOADING = 1 ;
     public PhoneAdapter(Context context, List<Product> listProduct) {
         this.context = context;
         this.listProduct = listProduct;
@@ -28,19 +30,37 @@ public class PhoneAdapter  extends RecyclerView.Adapter<PhoneAdapter.MyViewHolde
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_phone, parent, false);
-        return new MyViewHolder(item);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      if(viewType == VIEW_TYPE_DATA){
+          View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_phone, parent, false);
+          return new MyViewHolder(item);
+      }
+      else{
+          View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
+          return new LoadingViewHoler(item);
+      }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PhoneAdapter.MyViewHolder holder, int position) {
-        Product product = listProduct.get(position);
-        holder.name.setText(product.getName());
-        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
-        holder.price.setText("Giá "+ decimalFormat.format(product.getPrice())+ "VNĐ");
-        holder.description.setText(product.getDescription());
-        Glide.with(context).load(product.getImage()).into(holder.image);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof  MyViewHolder){
+            MyViewHolder myViewHolder = (MyViewHolder) holder;
+            Product product = listProduct.get(position);
+            myViewHolder.name.setText(product.getName());
+            DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+            myViewHolder.price.setText("Giá "+ decimalFormat.format(product.getPrice())+ "VNĐ");
+            myViewHolder.description.setText(product.getDescription());
+            Glide.with(context).load(product.getImage()).into(myViewHolder.image);
+        }
+        else{
+            LoadingViewHoler loadingViewHoler = (LoadingViewHoler) holder;
+            loadingViewHoler.progressBar.setIndeterminate(true);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return listProduct.get(position) == null ? VIEW_TYPE_lOADING: VIEW_TYPE_DATA;
     }
 
     @Override
@@ -57,6 +77,13 @@ public class PhoneAdapter  extends RecyclerView.Adapter<PhoneAdapter.MyViewHolde
             name = itemView.findViewById(R.id.item_phone_name);
             image = itemView.findViewById(R.id.item_phone_image);
             description = itemView.findViewById(R.id.item_phone_description);
+        }
+    }
+    public class LoadingViewHoler extends  RecyclerView.ViewHolder{
+        ProgressBar progressBar;
+        public LoadingViewHoler(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progressbar);
         }
     }
 }
