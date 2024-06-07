effe32593ec21477f21be794fe7963c0ce61655d
ThanhQuyen1 <21130508@st.hcmuaf.edu.vn>
2024-06-07 23:21:15 +0700

    ..

--- Android/app/src/main/java/com/example/appbanhang/utils/Utils.java
+++ Android/app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -5,6 +5,6 @@ import com.example.appbanhang.model.Cart;
 import java.util.List;
 
 public class Utils {
-    public  static  final String BASR_URL="http://192.168.75.1:8080/server_war/";
+    public  static  final String BASR_URL="http://192.168.1.132:8080/server_war/";
     public static List<Cart> carts;
 }
