f7c5bc8dabc6e643c8f80abdd632055b2054a56a
HUUQUY1004 <99793142+HUUQUY1004@users.noreply.github.com>
2024-06-10 02:04:23 +0700

    BE-get order

--- WebService/server/src/main/java/DAO/OrderDAO.java
+++ WebService/server/src/main/java/DAO/OrderDAO.java
@@ -1,11 +1,15 @@
 package DAO;
 
+import Model.CT_Order;
+import Model.Order;
 import Model.Product;
 import Model.User;
 import Services.Connect;
 import com.google.gson.Gson;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderDAO {
     Gson gson = new Gson();
@@ -128,5 +132,41 @@ public int mainOrder(String idUser, String address, String totalPrice) {
 
         }
         return  checkBool;
+    }
+    public List<Order> getOrderByIdUser(String idUser) {
+        Connection connection = null;
+        List<Order> orders = new ArrayList<>();
+        List<Product> products = new ArrayList<>();
+        try{
+            connection = Connect.getConnection();
+            String sql = "SELECT idOrder,address,phoneNumber, totalPrice, status,product.id as idProduct  ,name,image,type, price, quantity,description FROM orderproduct AS op JOIN ct_order AS ct ON op.id = ct.idOrder join product as product on ct.idProduct = product.id WHERE op.idUser = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt( 1,Integer.parseInt(idUser));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                Order order = new Order();
+                order.setId(resultSet.getInt("idOrder"));
+                order.setStatus(resultSet.getString("status"));
+                order.setAddress(resultSet.getString("address"));
+                order.setPhoneNumber(resultSet.getString("phoneNumber"));
+                order.setTotal(resultSet.getString("totalPrice"));
+                Product product = new Product();
+                CT_Order ctOrder = new CT_Order();
+                product.setName(resultSet.getString("name"));
+                product.setDescription(resultSet.getString("description"));
+                product.setCount(resultSet.getInt("quantity"));
+                product.setPrice(resultSet.getInt("price"));
+                product.setImage(resultSet.getString("image"));
+                product.setType(resultSet.getInt("type"));
+                product.setId(resultSet.getInt("idProduct"));
+                products.add(product);
+                order.setProducts(products);
+                orders.add(order);
+            }
+            return  orders;
+        }
+        catch (SQLException e) {
+            throw  new RuntimeException(e);
+        }
     }
 }
