68a94de4bf3fcf336c0eace469bde154a323962f
HUUQUY1004 <99793142+HUUQUY1004@users.noreply.github.com>
2024-06-12 02:11:09 +0700

    search component

---
+++ Android/app/src/main/java/com/example/appbanhang/activity/SearchActivity.java
@@ -0,0 +1,56 @@
+package com.example.appbanhang.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appbanhang.R;
+import com.example.appbanhang.retrofit.APIBanHang;
+
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+
+public class SearchActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    APIBanHang apiBanHang;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_search);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Mapping();
+        ActionToolBar();
+    }
+    private void ActionToolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    private void Mapping() {
+        toolbar = findViewById(R.id.toolbar_search);
+        recyclerView = findViewById(R.id.recyclerview_search);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+}
