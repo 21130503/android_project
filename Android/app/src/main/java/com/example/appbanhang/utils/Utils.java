c4d52e2bfe407621d7a01fb3548630bef3b7bb2a
NguyenSang2003 <21130512@st.hcmuaf.edu.vn>
2024-06-18 16:53:41 +0700

    fix loi login tren may local

--- Android/app/src/main/java/com/example/appbanhang/utils/Utils.java
+++ Android/app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -6,7 +6,8 @@ import com.example.appbanhang.model.User;
 import java.util.List;
 
 public class Utils {
-    public  static  final String BASR_URL="http://192.168.75.1:8080/server_war/";
+//    public  static  final String BASR_URL="http://192.168.75.1:8080/server_war/";
+    public  static  final String BASR_URL="http://192.168.43.233:8080/server_war/";
     public static List<Cart> carts;
     public static User currentUser = new User();
 }
