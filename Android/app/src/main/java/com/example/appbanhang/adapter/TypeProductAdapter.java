f573415fe9b548af58fc3a3657ff6de134801bca
NguyenSang2003 <21130512@st.hcmuaf.edu.vn>
2024-06-21 07:44:47 +0700

    Restore TypeProductAdapter

--- Android/app/src/main/java/com/example/appbanhang/adapter/TypeProductAdapter.java
+++ Android/app/src/main/java/com/example/appbanhang/adapter/TypeProductAdapter.java
@@ -14,9 +14,10 @@ import com.example.appbanhang.model.TypeProduct;
 
 import java.util.List;
 
+
 public class TypeProductAdapter extends BaseAdapter {
-    private List<TypeProduct> typeProductList;
-    private Context context;
+    List<TypeProduct> typeProductList;
+    Context context;
 
     public TypeProductAdapter(List<TypeProduct> typeProductList, Context context) {
         this.typeProductList = typeProductList;
@@ -25,41 +26,45 @@ public class TypeProductAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return typeProductList.size();
+        // Kiểm tra null trước khi gọi size()
+        if (typeProductList != null) {
+            return typeProductList.size();
+        } else {
+            return 0;
+        }
     }
 
     @Override
     public Object getItem(int position) {
-        return typeProductList.get(position);
+        return null;
     }
 
     @Override
     public long getItemId(int position) {
-        return position;
+        return 0;
+    }
+
+    public class ViewHolder {
+        TextView textNameProduct;
+        ImageView imageProduct;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.item_product, parent, false);
+        ViewHolder viewHolder = null;
+        if (viewHolder == null) {
             viewHolder = new ViewHolder();
-            viewHolder.textNameProduct = convertView.findViewById(R.id.item_name_product);
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.item_product, null);
             viewHolder.imageProduct = convertView.findViewById(R.id.item_image);
+            viewHolder.textNameProduct = convertView.findViewById(R.id.item_name_product);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-
-        TypeProduct typeProduct = typeProductList.get(position);
-        viewHolder.textNameProduct.setText(typeProduct.getName());
-        Glide.with(context).load(typeProduct.getImage()).into(viewHolder.imageProduct);
+        viewHolder.textNameProduct.setText(typeProductList.get(position).getName());
+        Glide.with(context).load(typeProductList.get(position).getImage()).into(viewHolder.imageProduct);
 
         return convertView;
-    }
-
-    static class ViewHolder {
-        TextView textNameProduct;
-        ImageView imageProduct;
     }
 }
